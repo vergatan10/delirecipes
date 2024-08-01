90f2118127a838bbc39d6909a1f2d94a0d6ef5b5
Verga Tandika <verga.tandika@gmail.com>
2024-08-02 03:26:56 +0700

    add search & postman & db dummy

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,15 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\RecipeController;
+use App\Http\Controllers\Api\SearchController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/recipes/search', [SearchController::class, 'index']);
 
 Route::get('/category/{category:slug}', [CategoryController::class, 'show']);
 Route::apiResource('/categories', CategoryController::class);
